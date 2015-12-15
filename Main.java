d8dc53d697bde3fe64718046d44865899fe3ad8d
LostLobos <igormorse@hotmail.com>
2015-12-15 20:31:52 -0200

    Introdução ao JDBC

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+import java.util.*;
+
 public class Main{
 	
 	public static void main(String Args[]){
+		
+		jdbc conn = new jdbc(jdbc.MySQL);
+		
+		conn.connect();
+		
+		if (conn.isConnected())
+			System.out.println("Conectado");
+		else
+			System.out.println("Não Conectado");
+		
+		conn.close();
+		
+		if (conn.isConnected())
+			System.out.println("Conectado");
+		else
+			System.out.println("Não Conectado");
+		
 		System.out.println("Hello World");
 	}
 }
---
+++ jdbc.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+
+public class jdbc{
+	
+	private Connection connection;
+	
+	private static final String URL = "jdbc:mysql://db4free.net:3306/projetolp2?useSSL=false";
+	private static final String USER = "projetolp2";
+	private static final String PASS = "projetojbdc";
+	
+	//private static final String URL = "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5100003?useSSL=false";
+	//private static final String USER = "sql5100003";
+	//private static final String PASS = "aEJtujANPc";
+	
+	public static String MySQL = "com.mysql.jdbc.Driver";
+	
+	public jdbc(String connType){
+		
+		try {
+			Class.forName(connType);
+		}
+		catch(Exception ex) {
+			System.out.println("Error: Não foi possível Carregar o Driver: " + ex.getMessage());
+			System.exit(1);
+		}
+	}
+	
+	public void connect(){
+		try{
+			connection = DriverManager.getConnection(URL,USER,PASS);
+		}catch (SQLException SQL_e){
+			System.out.println("Error: Não foi possível Estabelecer uma Conexão! " + SQL_e.getMessage());
+			System.exit(1);
+		}
+	}
+	
+	public void close(){
+		try{
+			connection.close();
+		}catch (SQLException SQL_e){
+			System.out.println("Error: Não foi possível Encerrar a Conexão! " + SQL_e.getMessage());
+			System.exit(1);
+		}
+	}
+	
+	public boolean isConnected(){
+		try{
+			return !connection.isClosed();
+		}catch(SQLException SQL_e){
+			return false;
+		}
+	}
+	
+	public Connection getConnection(){
+		return connection;
+	}
+	
+}
